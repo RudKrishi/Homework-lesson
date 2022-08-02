fab60471e45039bcd512bb963416ae424096dc1e
Di <diana.nikonorova1910@mail.ru>
2022-08-02 19:02:36 +0300

    выполнена задана 1 с урока 2, также оформлены выполненные задачи

--- Program.cs
+++ Program.cs
@@ -2,11 +2,11 @@
  // Задача 1 .Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 
 int number = new Random().Next(10,1000);
-int x = number / 100;
-int y = number / 1000;
-int z = number % 10;
- Console.WriteLine($ "y {y}");
-
+int x = number % 100;
+int y = x / 10;
+ Console.WriteLine($"Домашнее задание с Урока 2. Задача 1. number {number}");
+ Console.WriteLine($"y = {y}");
+ 
 
 
 
@@ -36,13 +36,13 @@ if (a < b)
 {
     min=a;
     max=b;
-Console.WriteLine($"Из чисел {a} и {b} max = {max}, min = {min}");
+Console.WriteLine($"Домашнее задание с Урока 1. Задача 1. Из чисел {a} и {b} max = {max}, min = {min}");
 }
 else
 {
 max=a;
 min=b;
-Console.WriteLine($"Из чисел {a} и {b} max = {max}, min = {min}");
+Console.WriteLine($"Задача 1. Из чисел {a} и {b} max = {max}, min = {min}");
 }
 
 
@@ -56,18 +56,18 @@ int f = new Random().Next(1,10);
 if ((d>e)&&(d>f))
 {
     max = d;
-Console.WriteLine($"Максимльное из чисел {d} и {e} и {f} max = {max}");
+Console.WriteLine($"Задача 2. Максимльное из чисел {d} и {e} и {f} max = {max}");
 }
 else
 {
     if (d<f)
     {
     max = f;
-Console.WriteLine($"Максимльное из чисел {d} и {e} и {f} max = {max}");
+Console.WriteLine($"Задача 2. Максимльное из чисел: {d} и {e} и {f} max = {max}");
     }
     if(e>f)
     max = e;
-    Console.WriteLine($"Максимльное из чисел {d} и {e} и {f} max = {max}");
+    Console.WriteLine($"Задача 2. Максимльное из чисел: {d} и {e} и {f} max = {max}");
 }
 
 
@@ -75,10 +75,10 @@ Console.WriteLine($"Максимльное из чисел {d} и {e} и {f} max
 
 int N = new Random().Next(1,100);
 if (N % 2 == 0)
- {Console.WriteLine($" {N}  - четное число");
+ {Console.WriteLine($"Задача 3: {N}  - четное число");
  }
  else 
  {
-    Console.WriteLine($" {N}  - нечетное число");
+    Console.WriteLine($"Задача 3: {N}  - нечетное число");
  }
 
