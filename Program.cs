d96a1f62b3b159db5593d419ce6e1098f7da5de3
Di <diana.nikonorova1910@mail.ru>
2022-08-04 18:49:06 +0300

    выполнена задача 2 с урока 3

--- Program.cs
+++ Program.cs
@@ -1,71 +1,192 @@
-﻿ // Домашнее задание с Урока 2.
- //Массивы и Функции в программировании.(блок-схемы в папке ex02.drawio.png)
- // Задача 1. Напишите программу, которая принимает на вход трёхзначное число 
- //и на выходе показывает вторую цифру этого числа.
+﻿// Домашнее задание с урока 3.(блок-схемы в папке ex03.drawio.png)
+//Задача 1. Напишите программу, 
+//которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 
-int number = new Random().Next(10,1000);
-int x = number % 100;
-int y = x / 10;
- Console.WriteLine($"Домашнее задание с Урока 2");
- Console.WriteLine($"Задача 1. number: {number}");
- Console.WriteLine($"y = {y}");
+// int N = 0;
+// int a = 0;
+// int b = 0;
+// int b1 = 0;
+// int c = 0;
+// int c1 = 0;
+// int d = 0;
+// int N2 = 0;
+// int a2 = 0;
+// int b2 = 0;
+// int c2 = 0;
+// int d2 = 0;
+// int e = 0;
+// int e2 = 0;
+// int e3 = 0;
+
+// Console.Write($"Введите пятизначное число: ");
+// N = Convert.ToInt32(Console.ReadLine());
+// if(N < 100000 && N > 10000)
+// {
+//     a = N / 10000;
+//     b = N / 1000;
+//     b1 = b % 10;
+//     e = N / 100;
+//     e2 = e / 10;
+//     e3 = e2 % 10;
+//     c = N % 100;
+//     c1 = c / 10;
+//     d = N % 10;
+
+// if (a2 = d)
+// {
+//     if (b2 = c1)
+
+// {
+//     if (c2 = c1)
+//     {
+//         if (d2 = a)
+//         {
+
+ 
+// N2 = (a2 * 10000) + (b2 * 1000) + c2 + d2;
+    
+//     Console.WriteLine($" Введенное число N: {N}");
+//     Console.WriteLine($" а {a}, b {b1}, c {c1}, d {d} ");
+//     Console.WriteLine($" Перевернутое число N2: {N2}");
+//      Console.WriteLine($" e{e}, e2{e2}, e3{e3}");
+//             }
+//     }
+// }
+
+// }
+// }
+// else
+// {
+//     Console.WriteLine($"Введено неправильное число ");
+// }
+
+
+
+//Задача 2.
+//Напишите программу, которая принимает на вход координаты двух точек и
+ //находит расстояние между ними в 3D пространстве.
+double x1 = 0;
+double y1 = 0;
+double z1 = 0;
+double x2 = 0;
+double y2 = 0;
+double z2 = 0;
+double dis = 0;
+Console.Write($"Задача 2.");
+Console.Write($"Введите координаты для A: ");
+//A
+
+ x1 = Convert.ToInt32(Console.ReadLine());
+ y1 = Convert.ToInt32(Console.ReadLine());
+ z1 = Convert.ToInt32(Console.ReadLine());
+
+ Console.Write($"Введите координаты для B: ");
+//B
+ x2 = Convert.ToInt32(Console.ReadLine());
+ y2 = Convert.ToInt32(Console.ReadLine());
+ z2 = Convert.ToInt32(Console.ReadLine());
+
+
+ dis = Math.Sqrt(((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1) + (z2 - z1)*(z2 - z1)));
+
+
+Console.Write($"Расстояние меджу А и И: {dis}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Задача 3. 
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// int N = 1;
+// int i = 1;
+// int a = 0;
+// Console.Write($"Введите число: ");
+// N = Convert.ToInt32(Console.ReadLine());
+
+// while (i < N)
+// {
+//     a = N - 1;
+//     a = a*a*a;
+//     i++;
+//     Console.WriteLine(a);
+    
+// }
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+//  // Домашнее задание с Урока 2.(ВЫПОЛНЕНО)
+//  //Массивы и Функции в программировании.(блок-схемы в папке ex02.drawio.png)
+//  // Задача 1. Напишите программу, которая принимает на вход трёхзначное число 
+//  //и на выходе показывает вторую цифру этого числа.
+
+// int number = new Random().Next(10,1000);
+// int x = number % 100;
+// int y = x / 10;
+//  Console.WriteLine($"Домашнее задание с Урока 2");
+//  Console.WriteLine($"Задача 1. number: {number}");
+//  Console.WriteLine($"y = {y}");
  
 
-//Задача 2: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// //Задача 2: Напишите программу, которая выводит третью цифру заданного числа или 
+// //сообщает, что третьей цифры нет.
 
 
-int number1 = new Random().Next(10,1000);
-int z1 = number1 % 10;
-if(number1 < 100)
-{
-    Console.WriteLine($"Задача 2. number1: {number1}");
-    Console.WriteLine($"Третьей цифры нет");
+// int number1 = new Random().Next(10,1000);
+// int z1 = number1 % 10;
+// if(number1 < 100)
+// {
+//     Console.WriteLine($"Задача 2. number1: {number1}");
+//     Console.WriteLine($"Третьей цифры нет");
     
-}
+// }
 
-else
-{
+// else
+// {
 
-Console.WriteLine($"Задача 2. number1: {number1}");
-Console.WriteLine($"z1 = {z1}");
-}
+// Console.WriteLine($"Задача 2. number1: {number1}");
+// Console.WriteLine($"z1 = {z1}");
+// }
 
-//Напишите программу, которая принимает на вход цифру,
-//обозначающую день недели, и проверяет, является ли этот день выходным.
+// //Задача 3. 
+// //Напишите программу, которая принимает на вход цифру,
+// //обозначающую день недели, и проверяет, является ли этот день выходным.
 
-int number2 = new Random().Next(-10,10);
-if (number2 >1 && number2 < 6)
-{
-Console.WriteLine($"Домашнее задание с Урока 2. Задача 3.");
- Console.WriteLine($"number2: {number2}");
-Console.WriteLine($"Это рабочий день недели");
-}
+// int number2 = new Random().Next(-10,10);
+// if (number2 >1 && number2 < 6)
+// {
+// Console.WriteLine($"Домашнее задание с Урока 2. Задача 3.");
+//  Console.WriteLine($"number2: {number2}");
+// Console.WriteLine($"Это рабочий день недели");
+// }
 
-    if(number2 ==7)
-    {
-    Console.WriteLine($"Домашнее задание с Урока 2. Задача 3.");
-     Console.WriteLine($"number2: {number2}");
-    Console.WriteLine($"Это выходной день недели");
-    }
-    if(number2 ==6)
-    {
-    Console.WriteLine($"Домашнее задание с Урока 2. Задача 3.");
-     Console.WriteLine($"number2: {number2}");
-    Console.WriteLine($"Это выходной день недели");
-    }
-    if(number2 < 1 && number2 > 7)
-{
-   Console.WriteLine($"Домашнее задание с Урока 2. Задача 3.");
-   Console.WriteLine($"number2: {number2}");
-   Console.WriteLine($"Такого дня недели нет"); 
-}
-
-
-
-
-
-
-
+//     if(number2 ==7)
+//     {
+//     Console.WriteLine($"Домашнее задание с Урока 2. Задача 3.");
+//      Console.WriteLine($"number2: {number2}");
+//     Console.WriteLine($"Это выходной день недели");
+//     }
+//     if(number2 ==6)
+//     {
+//     Console.WriteLine($"Домашнее задание с Урока 2. Задача 3.");
+//      Console.WriteLine($"number2: {number2}");
+//     Console.WriteLine($"Это выходной день недели");
+//     }
+//     if(number2 < 1 && number2 > 7)
+// {
+//    Console.WriteLine($"Домашнее задание с Урока 2. Задача 3.");
+//    Console.WriteLine($"number2: {number2}");
+//    Console.WriteLine($"Такого дня недели нет"); 
+// }
 
 
 
