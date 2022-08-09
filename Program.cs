59cbaa695113ee17ccafca45c941bde432930f0f
Di <diana.nikonorova1910@mail.ru>
2022-08-09 16:23:09 +0300

    решена 2я задача с урока 4

--- Program.cs
+++ Program.cs
@@ -2,12 +2,30 @@
 // Зачада 1. Задача 25: Напишите метод, который 
 //принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 
-int a = 0;
-int b = 0;
-int N = 0;
-Console.Write($"Введите возводимое число: ");
-a = Convert.ToInt32(Console.ReadLine());
-Console.Write($"Введите число, в которое будем возводить: ");
-b = Convert.ToInt32(Console.ReadLine());
-N = Convert.ToInt32(Math.Pow(a, b));
-Console.WriteLine($"Итоговое  число: {N}");
+// int a = 0;
+// int b = 0;
+// int N = 0;
+// Console.Write($"Введите возводимое число: ");
+// a = Convert.ToInt32(Console.ReadLine());
+// Console.Write($"Введите число, в которое будем возводить: ");
+// b = Convert.ToInt32(Console.ReadLine());
+// N = Convert.ToInt32(Math.Pow(a, b));
+// Console.WriteLine($"Итоговое  число: {N}");
+
+
+
+//Зачада 2. Задача 27: Напишите метод, который принимает 
+//на вход число и выдаёт сумму цифр в числе.
+
+//Зачада 3. Задача 29: Напишите метод(ы), который задаёт массив из 8 элементов
+// и выводит их на экран.
+
+int length = 8;
+char [] arg = new char[length];
+ int i = 0;
+ while ( i < length)
+ {
+    arg[i] = Convert.ToChar(new Random().Next(64, 87));
+    Console.WriteLine(arg[i]);
+    i++;
+ }
