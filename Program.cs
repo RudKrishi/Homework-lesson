cee2eaa6673fcbb6d336dcd9dc628c1d47d09c22
Di <diana.nikonorova1910@mail.ru>
2022-08-29 20:52:01 +0300

    решена задача из урока 7

--- Program.cs
+++ Program.cs
@@ -1,17 +1,47 @@
 ﻿// // Домашнее задание с урока 6.
 //Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 
-Console.Write("Введите числа(через пробел): ");
-int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-int count = 0;
+// Console.Write("Введите числа(через пробел): ");
+// int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+// int count = 0;
  
-for (int i = 0; i < array.Length; i++)
-{
-    if (array[i] > 0)
-    {
-        count++;
-    }
-}
+// for (int i = 0; i < array.Length; i++)
+// {
+//     if (array[i] > 0)
+//     {
+//         count++;
+//     }
+// }
  
-Console.WriteLine($"Количество чисел: {count}");
+// Console.WriteLine($"Количество чисел: {count}");
 
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+// // Домашнее задание с урока 6.
+
+//Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// double[,] table = new double [2,5];
+// Random random = new Random();
+// for (int rows = 0; rows < 2; rows++)
+// {
+//     for (int columns = 0; columns < 5; columns++)
+//     {
+//     Console.WriteLine($"-{table[rows, columns]}-");
+//     }
+
+// }
+
+
+double[,] a = new double[3, 4];
+Random random = new Random();
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        a[i, j] = random.Next(100);
+        Console.Write("{0,3}", a[i, j]);
+    }
+    Console.WriteLine();
+}
