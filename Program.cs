16821e35207a0997f92c63be97f701fecd0ca61e
Di <diana.nikonorova1910@mail.ru>
2022-08-29 20:03:59 +0300

    решена задача из урока 6

--- Program.cs
+++ Program.cs
@@ -1,71 +1,17 @@
-﻿// // Домашнее задание с урока 5.
-//Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
-//Напишите программу, которая покажет количество чётных чисел в массиве.
+﻿// // Домашнее задание с урока 6.
+//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 
-
-// void FillArray(int[] array)
-// {
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//     array[i]  = new Random().Next(1, 100);
-//     }
-// }
-
-// int [] array = new int[100];
-// FillArray(array);
-// int index = 0;
-// int a = 0;
-
-// while(index < array.Length)
-// {
-//     if (array[index] % 2 == 0)
-//     {    
-//    index++;
-//    a = a + 1;
-//    }
-// else
-// {
-// index++;
-//     a=a+0;
-// }
-// }
-// Console.WriteLine(a);
-
-//Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
-
-
-void FillArray(int[] array)
-{
-
+Console.Write("Введите числа(через пробел): ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+ 
 for (int i = 0; i < array.Length; i++)
 {
-    array[i]  = new Random().Next(1, 10);
-}
-}
-int [] array = new int[9];
-FillArray (array);
-int index = 0;
-int sum = 0;
-
-while(index < array.Length)
-{
-    if (index % 2 == 1)
-{
-    sum = sum + array[index];
-    index++;
-}
-    else
+    if (array[i] > 0)
     {
-        sum = sum + 0;
-        index++;
+        count++;
     }
 }
-
-//Console.Write(array[index]);
-Console.WriteLine(sum);
-
-
-
-
-//Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+ 
+Console.WriteLine($"Количество чисел: {count}");
 
